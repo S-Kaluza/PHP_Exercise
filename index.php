837d54de35f057f461b8af9934c96d60aff94935
Szymon Kałuża <szymon.i.kaluza@gmail.com>
2022-04-05 12:28:28 +0200

    Pierwszy plik -- Lorem ipsum

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="pl">
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis orci quam.
+                    Maecenas pulvinar magna at odio volutpat pellentesque. Nullam finibus ut nulla eget facilisis.
+                    Aenean convallis lobortis tortor eget sodales. Morbi erat massa, placerat ut massa id, tempor volutpat libero.
+                    Mauris euismod, enim sit amet pharetra auctor, velit est rhoncus libero,
+                    id feugiat metus ante eu ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                    Phasellus tempus, elit nec porttitor maximus, ipsum sapien condimentum risus, id finibus justo ante in nibh. Duis auctor bibendum leo,
+                    non scelerisque turpis. Fusce at tellus tellus. Maecenas a convallis massa. Aenean pretium viverra erat, 
+                    eleifend placerat diam ultricies sit amet. Maecenas molestie dignissim sem,
+                 at bibendum diam imperdiet at. Donec ut mauris pharetra, facilisis elit ac, finibus orci.
+                </div>
+                <div class="col-6">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis orci quam.
+                Maecenas pulvinar magna at odio volutpat pellentesque. Nullam finibus ut nulla eget facilisis.
+                Aenean convallis lobortis tortor eget sodales. Morbi erat massa, placerat ut massa id, tempor volutpat libero.
+                Mauris euismod, enim sit amet pharetra auctor, velit est rhoncus libero,
+                id feugiat metus ante eu ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                Phasellus tempus, elit nec porttitor maximus, ipsum sapien condimentum risus, id finibus justo ante in nibh. Duis auctor bibendum leo,                    non scelerisque turpis. Fusce at tellus tellus. Maecenas a convallis massa. Aenean pretium viverra erat, 
+                eleifend placerat diam ultricies sit amet. Maecenas molestie dignissim sem,
+                 at bibendum diam imperdiet at. Donec ut mauris pharetra, facilisis elit ac, finibus orci.
+                </div>
+                <div class="col-3">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis orci quam.
+                Maecenas pulvinar magna at odio volutpat pellentesque. Nullam finibus ut nulla eget facilisis.
+                Aenean convallis lobortis tortor eget sodales. Morbi erat massa, placerat ut massa id, tempor volutpat libero.
+                Mauris euismod, enim sit amet pharetra auctor, velit est rhoncus libero,
+                id feugiat metus ante eu ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+                Phasellus tempus, elit nec porttitor maximus, ipsum sapien condimentum risus, id finibus justo ante in nibh. Duis auctor bibendum leo,
+                non scelerisque turpis. Fusce at tellus tellus. Maecenas a convallis massa. Aenean pretium viverra erat, 
+                eleifend placerat diam ultricies sit amet. Maecenas molestie dignissim sem,
+                 at bibendum diam imperdiet at. Donec ut mauris pharetra, facilisis elit ac, finibus orci.
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
