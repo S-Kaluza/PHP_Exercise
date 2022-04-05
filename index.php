34820d5b70ae48ad242cf8dade8853abdc943bb2
Szymon Kałuża <szymon.i.kaluza@gmail.com>
2022-04-05 12:36:05 +0200

    Another picsum

--- index.php
+++ index.php
@@ -19,16 +19,8 @@
                 eleifend placerat diam ultricies sit amet. Maecenas molestie dignissim sem,
                  at bibendum diam imperdiet at. Donec ut mauris pharetra, facilisis elit ac, finibus orci.
                 </div>
-                <div class="col-3">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis orci quam.
-                Maecenas pulvinar magna at odio volutpat pellentesque. Nullam finibus ut nulla eget facilisis.
-                Aenean convallis lobortis tortor eget sodales. Morbi erat massa, placerat ut massa id, tempor volutpat libero.
-                Mauris euismod, enim sit amet pharetra auctor, velit est rhoncus libero,
-                id feugiat metus ante eu ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                Phasellus tempus, elit nec porttitor maximus, ipsum sapien condimentum risus, id finibus justo ante in nibh. Duis auctor bibendum leo,
-                non scelerisque turpis. Fusce at tellus tellus. Maecenas a convallis massa. Aenean pretium viverra erat, 
-                eleifend placerat diam ultricies sit amet. Maecenas molestie dignissim sem,
-                 at bibendum diam imperdiet at. Donec ut mauris pharetra, facilisis elit ac, finibus orci.
+                <div class="col-3 p-3">
+                    <img src="https://picsum.photos/300/700" alt=""/>
                 </div>
             </div>
         </div>
